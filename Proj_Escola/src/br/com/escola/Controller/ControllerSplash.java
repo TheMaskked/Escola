e494823f1034b210a02ee141d33cb3af1865720c
lfnoleto <lucasnoleto18@gmail.com>
2016-12-09 00:40:35 -0200

    Class Controller da Splash que vem antes da tela de login

---
+++ Proj_Escola/src/br/com/escola/Controller/ControllerSplash.java
@@ -0,0 +1,19 @@
+package br.com.escola.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Lucas-PC on 08/12/2016.
+ * aqui controller da imagem inicial que e chamada de splash
+ */
+public class ControllerSplash implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+}
