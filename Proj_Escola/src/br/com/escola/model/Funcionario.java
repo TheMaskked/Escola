626d81c30c2b26332ca921ab746556e5f8e1ea85
taffarel55 <mauruciotaffarel@gmail.com>
2016-12-10 19:05:18 -0300

    Implementação da super classe funcionário com seus respectivos atributos e métodos setters e getters

--- Proj_Escola/src/br/com/escola/model/Funcionario.java
+++ Proj_Escola/src/br/com/escola/model/Funcionario.java
@@ -1,7 +1,35 @@
 package br.com.escola.model;
 
 // Classe funcionario
-public class Funcionario {
-
-	//TODO Definir atributos e seus respectivos m�todos getters e setters...
+abstract class Funcionario {
+	private String name;
+	private int age;
+	private float salary;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String nam) {
+		this.name=nam;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int ag){
+		if (ag>0&&ag<120) {
+			this.age=ag;
+		}
+	}
+	
+	public float getSalary(){
+		return this.salary;
+	}
+	
+	public void setAge(float sal) {
+		this.salary=sal;
+	}
+	
 }
